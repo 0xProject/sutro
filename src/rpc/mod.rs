ea5850b80f3ddd2faa0bcd00738e6c59a88c5e8c
Remco Bloemen <remco@0x.org>
2020-10-19 15:14:35 -0700

    Add logging middleware

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,26 +1,55 @@
 //! Implements a partial Ethereum JSON-RPC interface.
 //!
 //! See <https://eth.wiki/json-rpc/API>
+//!
+//! Uses <https://github.com/paritytech/jsonrpc>
 
 use crate::evm_jit::Program;
-use jsonrpc_http_server::{jsonrpc_core::*, *};
+use jsonrpc_core::{
+    futures::{future::Either, Future},
+    middleware, FutureResponse, MetaIoHandler, Metadata, Middleware, Params, Request, Response,
+};
+use jsonrpc_http_server::{AccessControlAllowOrigin, DomainsValidation, ServerBuilder};
 use log::info;
-use serde_json::json;
+use serde_json::{json, Value};
+use std::{
+    sync::atomic::{self, AtomicUsize},
+    time::Instant,
+};
 
-struct Logger;
+#[derive(Clone, Debug, Default)]
+struct Meta(usize);
+impl Metadata for Meta {}
 
-impl RequestMiddleware for Logger {
-    fn on_request(&self, request: hyper::Request<hyper::Body>) -> RequestMiddlewareAction {
-        info!("REQ {:?}", request.body());
-        RequestMiddlewareAction::Proceed {
-            should_continue_on_invalid_cors: false,
-            request,
-        }
+// See <https://github.com/paritytech/jsonrpc/blob/v15.1.0/core/examples/middlewares.rs>
+#[derive(Default)]
+struct Logger(AtomicUsize);
+impl Middleware<Meta> for Logger {
+    type CallFuture = middleware::NoopCallFuture;
+    type Future = FutureResponse;
+
+    fn on_request<F, X>(&self, request: Request, meta: Meta, next: F) -> Either<Self::Future, X>
+    where
+        F: FnOnce(Request, Meta) -> X + Send,
+        X: Future<Item = Option<Response>, Error = ()> + Send + 'static,
+    {
+        let start = Instant::now();
+        let request_number = self.0.fetch_add(1, atomic::Ordering::SeqCst);
+        println!(
+            "Processing request {}: {:?}, {:?}",
+            request_number, request, meta
+        );
+
+        Either::A(Box::new(next(request, meta).map(move |res| {
+            println!("Processing took: {:?}", start.elapsed());
+            res
+        })))
     }
 }
 
 pub fn main() {
-    let mut io = IoHandler::default();
+    let mut io = MetaIoHandler::with_middleware(Logger::default());
+
     io.add_method("say_hello", |_| Ok(json!("hello")));
     io.add_method("web3_clientVersion", |_| {
         Ok(json!(format!(
@@ -76,7 +105,6 @@ pub fn main() {
         .cors(DomainsValidation::AllowOnly(vec![
             AccessControlAllowOrigin::Null,
         ]))
-        .request_middleware(Logger)
         .start_http(&"127.0.0.1:8545".parse().unwrap())
         .expect("Unable to start RPC server");
 
